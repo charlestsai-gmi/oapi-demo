ab9c8ec7672448075deac3993e0d957a5f9d1c76
charles <charles.t@gmitec.net>
2024-11-29 14:36:59 +0800

    enable validator

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/charlestsai-gmi/oapi-demo/api"
 	"github.com/go-chi/chi/v5"
+	middleware "github.com/oapi-codegen/nethttp-middleware"
 )
 
 func main() {
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+		os.Exit(1)
+	}
+	// Clear out the servers array in the swagger spec, that skips validating
+	// that server names match. We don't know how this thing will be run.
+	swagger.Servers = nil
+
 	// create a type that satisfies the `api.ServerInterface`, which contains an implementation of every operation from the generated code
 	server := api.NewServer()
 
 	r := chi.NewMux()
+
+	// Use our validation middleware to check all requests against the
+	// OpenAPI schema.
+	r.Use(middleware.OapiRequestValidator(swagger))
 
 	// get an `http.Handler` that we can use
 	h := api.HandlerFromMux(server, r)
