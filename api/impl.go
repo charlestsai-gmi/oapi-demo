ab7b5a2f276218a788607c6ea0de913ff2529507
charles <charles.t@gmitec.net>
2024-11-29 13:57:41 +0800

    step4: impl chi
    chi example, https://github.com/oapi-codegen/oapi-codegen?tab=readme-ov-file#chi
    
    $go run main.go
    $curl localhost:8080/ping

---
+++ api/impl.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// optional code omitted
+
+type Server struct{}
+
+func NewServer() Server {
+	return Server{}
+}
+
+// (GET /ping)
+func (Server) GetPing(w http.ResponseWriter, r *http.Request) {
+	resp := Pong{
+		Ping: "pong",
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(resp)
+}
